8a8f1b371274c3599e920e7b40e184d471f4263a
mushegharustamyan <mushegh.arustamyan@gmail.com>
2023-04-04 17:22:56 +0400

    database creation and connecting

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express")
+const cors = require("cors")
+const connectionInit = require("./db/init")
 
 const app = express()
 
+app.use(cors({ origin: "*" }))
+app.use(express.json())
+
 app.listen(5000 , () => {
   console.log("listen port 5000")
+  connectionInit()
+  .then(_ => console.log("Database conected"))
 })
