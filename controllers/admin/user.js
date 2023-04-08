231eaad8c8e97dac299f0ac2a50defba6ff059ae
iammaximous <maxmanukian04@gmail.com>
2023-04-09 01:40:40 +0400

    Add admin permission check, user creation/update, show, and index functions with express-validator

--- controllers/admin/user.js
+++ controllers/admin/user.js
@@ -1,27 +1,74 @@
-const bcrypt = require("bcrypt")
-const { User } = require("../../db/sequelize")
+const bcrypt = require("bcrypt");
+const { User } = require("../../db/sequelize");
 
-const { sendResStatus } = require("../../utils/helpers")
+const { sendResStatus, sendResBody } = require("../../utils/helpers");
 
-exports.create = async (req , res) => {
-    const {name , password , login , roleId} = req.body
+exports.create = async (req, res) => {
+  const { name, password, login, roleId } = req.body;
 
-    const hashedPwd = await bcrypt.hash(password, 8)
+  const hashedPwd = await bcrypt.hash(password, 8);
 
-    User.create({
-        name,
-        password: hashedPwd,
-        login,
-        roleId
-    })
+  User.create({
+    name,
+    password: hashedPwd,
+    login,
+    roleId,
+  })
     .then((_) => sendResStatus(res, 201, "User Created"))
-    .catch((_) => sendResStatus(res, 500))
-}
+    .catch((_) => sendResStatus(res, 500));
+};
 
-exports.delete = (req , res) => {
-    const {id} = req.params
+exports.delete = (req, res) => {
+  const { id } = req.params;
 
-    User.destroy({where: {id}})
-    .then((_) => sendResStatus(res , 204))
-    .catch((_) => sendResStatus(res, 500))
-}
+  User.destroy({ where: { id } })
+    .then((_) => sendResStatus(res, 204))
+    .catch((_) => sendResStatus(res, 500));
+};
+
+const removeNullOrUndefined = (obj) => {
+  return Object.entries(obj).reduce((acc, [key, value]) => {
+    if (value !== null && value !== undefined) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+};
+
+exports.update = (req, res) => {
+  const { id } = req.params;
+  const { name, password, login, roleId } = req.body;
+  const filter = removeNullOrUndefined({
+    name,
+    password,
+    login,
+    password,
+    roleId,
+  });
+  console.log(filter);
+  User.findOne({ where: filter })
+    .then((user) => {
+      if (user) return sendResStatus(res, 409), "User already exist";
+    })
+    .then((_) => {
+      User.update(filter, {
+        where: { id },
+      })
+        .then((_) => sendResStatus(res, 201, "Record updated"))
+        .then((_) => sendResStatus(res, 500));
+    });
+};
+
+exports.index = (req, res) => {
+  User.findAll()
+    .then((result) => sendResBody(res, 200, result))
+    .catch((_) => sendResStatus(res, 500));
+};
+
+exports.show = (req, res) => {
+  const { id } = req.params;
+
+  User.findByPk(id)
+    .then((result) => sendResBody(res, 200, result))
+    .catch((_) => sendResStatus(res, 500));
+};
--- routes/admin/user.js
+++ routes/admin/user.js
@@ -1,12 +1,67 @@
-const express = require("express")
-const router = express.Router()
+const { check } = require("express-validator");
+const express = require("express");
+const router = express.Router();
 
 // const verifyCreate = require("../../"s)
+const verifyAdmin = require("../auth");
+const adminUserController = require("../../controllers/admin/user");
+const {
+  verifyCreate,
+  verifyUser,
+  verifyIsNotAdmin,
+} = require("../../middlewares/admin/user");
 
-const adminUserController = require("../../controllers/admin/user")
-const { verifyCreate, verifyUser, verifyIsNotAdmin } = require("../../middlewares/admin/user")
+router.post(
+  "/user",
+  [
+    check("name").notEmpty().withMessage("User name cannot be empty"),
 
-router.post("/user", verifyCreate , adminUserController.create)
-router.delete("/user/:id", [verifyUser , verifyIsNotAdmin] ,adminUserController.delete)
+    check("login")
+      .notEmpty()
+      .withMessage("Login cannot be empty")
+      .isLength({ min: 4 })
+      .withMessage("Login must be at least 5 charachters"),
 
-module.exports = router
+    check("password")
+      .notEmpty()
+      .withMessage("Password cannot be Empty")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 charachters"),
+  ],
+  [verifyAdmin, verifyCreate],
+  adminUserController.create
+);
+router.delete(
+  "/user/:id",
+  [verifyAdmin, verifyUser],
+  adminUserController.delete
+);
+router.put(
+  "/user/:id",
+  [
+    check("name")
+      .optional()
+      .notEmpty()
+      .withMessage("User name cannot be empty"),
+
+    check("login")
+      .optional()
+      .notEmpty()
+      .withMessage("Login cannot be empty")
+      .isLength({ min: 4 })
+      .withMessage("Login must be at least 5 characters"),
+
+    check("password")
+      .optional()
+      .notEmpty()
+      .withMessage("Password cannot be empty")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters"),
+  ],
+  [verifyAdmin, verifyUser, ],
+  adminUserController.update
+);
+router.get("/user", verifyAdmin, adminUserController.index);
+router.get("/user/:id", [verifyAdmin, verifyUser], adminUserController.show);
+
+module.exports = router;
