bf2bbe2f1dc83ab80d7fbb7777614297aba272ca
iammaximous <maxmanukian04@gmail.com>
2023-04-29 20:52:21 +0400

    changing jwt sign logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,32 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require("bcrypt")
-const {User} = require('../db/sequelize')
-const { sendResStatus, getEnv, sendResBody } = require('../utils/helpers')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../db/sequelize");
+const { sendResStatus, getEnv, sendResBody } = require("../utils/helpers");
 
-exports.signin = (req,res) => {
-    const { login , password} = req.body
-    User.findOne({
-        where: {
-            login
-         }
-    }) 
-        .then(async user => {
-            const decoded = await bcrypt.compare(password , user.password)
-            if(!user || !decoded){
-                return sendResStatus(res,409 , "Invalid Login or Password")
-            }
-            const token = jwt.sign({id : user.id}, getEnv("JWT_SECRET"), {
-                expiresIn: 80000
-            })
-            const roleId = user.roleId
+exports.signin = (req, res) => {
+  const { login, password } = req.body;
+  User.findOne({
+    where: {
+      login,
+    },
+  })
+    .then(async (user) => {
+      if (!user) {
+        return sendResStatus(res, 409, "Invalid Login or Password");
+      }
+      const decoded = await bcrypt.compare(password, user.password);
+      if (!decoded) {
+        return sendResStatus(res, 409, "Invalid Login or Password");
+      }
+      const token = jwt.sign(
+        { id: user.id, name: user.name, roleId: user.roleId },
+        getEnv("JWT_SECRET"),
+        {
+          expiresIn: 80000,
+        }
+      );
 
-            return res.send({token,roleId})
-        })
-        .catch(_ => sendResBody(res, 500,))
-}
+      return sendResBody(res, 200, { token });
+    })
+    .catch((_) => sendResStatus(res, 500, "Internal Server Error"));
+};
