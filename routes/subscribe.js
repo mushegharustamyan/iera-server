09f095f030f56c2b05203077b06567c9a462cc7d
iammaximous <maxmanukian04@gmail.com>
2023-05-09 20:07:31 +0400

    upd

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -2,23 +2,21 @@ const express = require("express");
 const router = express.Router();
 const { Subscribe } = require("../db/sequelize");
 
-const {verifyAdmin} = require("../middlewares/verifications")
+const { verifyAdmin } = require("../middlewares/verifications");
 
 const subscribeController = require("../controllers/subscribe");
 const { sendResStatus } = require("../utils/helpers");
 
-router.post(
-  "/subscribe",
-  (req, res, next) => {
-    const email = req.body.email;
-    Subscribe.findAll({ where: { email } }).then(({email}) => {
-      if (email) return sendResStatus(res,409);
-    });
-    next()
-  },
-  subscribeController.create
+router.post("/subscribe", subscribeController.create);
+router.get("/admin/subscribers", verifyAdmin, subscribeController.index);
+router.delete(
+  "/admin/subscribers/:id",
+  verifyAdmin,
+  subscribeController.delete
 );
-router.get("/admin/subscribers",verifyAdmin , subscribeController.index);
-router.delete("/admin/subscribers/:id", verifyAdmin,subscribeController.delete)
-router.get("/admin/subscribers/download",verifyAdmin,subscribeController.downloadAll )
+router.get(
+  "/admin/subscribers/download",
+  verifyAdmin,
+  subscribeController.downloadAll
+);
 module.exports = router;
